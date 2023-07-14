c2b3ab2cf703d319bcd896e41ab54232f10ec6b0
Parth Govale <133950391+Demolus13@users.noreply.github.com>
2023-07-14 18:29:46 +0530

    Let's go

---
+++ Home/home.js
@@ -0,0 +1,34 @@
+let aml = document.getElementById('aml');
+let waw = document.getElementById('waw');
+let L1 = document.querySelectorAll('.L1');
+let L2 = document.querySelectorAll('.L2');
+let spans = document.querySelectorAll('.waw-span');
+
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
+
+window.addEventListener("scroll", () => {
+    let scroll = this.scrollY;
+    console.log(scroll);
+    
+    if (scroll < 650) {
+        var s1 = 1 - scroll/1500;
+        var s2 = 1 - scroll/3000;
+        var s3 = 1 - scroll/550;
+        L1.forEach(l => {
+            l.style.transform = 'scale(' + s1 + ')';
+        });
+        L2.forEach(l => {
+            l.style.transform = 'scale(' + s2 + ')';
+        });
+        aml.style.opacity = s3;
+        waw.style.opacity = 1 - s3;
+    }
+
+    if (scroll > 650 && scroll < 1250) {
+        spans.forEach(span => {
+            span.style.height = 0 + '%';
+        });
+    }
+});
